e8b0e9d5e308b5c33e3469ca0872902619c40412
shivamsouravjha <shivam.jha@smallcase.com>
2023-05-21 00:25:22 +0530

    fixing controller

--- routes/source.js
+++ routes/source.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import SourceController from '../app/Controllers/sourceController';
+import SourceController from '../app/Controllers/sourceController.js';
 
 const SourceApiRouter = express.Router();
 
