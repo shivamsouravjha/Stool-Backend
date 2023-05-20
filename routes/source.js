54c973484437c2360934cf66b5b51338974fb60c
shivamsouravjha <shivam.jha@smallcase.com>
2023-05-21 00:27:36 +0530

    controller

--- routes/source.js
+++ routes/source.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import SourceController from '../app/Controllers/sourceController.js';
+import SourceController from './../app/Controllers/sourceController';
 
 const SourceApiRouter = express.Router();
 
